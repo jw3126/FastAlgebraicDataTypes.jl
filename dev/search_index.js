f179fd1299cc58eae5df189eb058d7c8f6f23301
Documenter.jl <documenter@juliadocs.github.io>
2022-07-06 14:55:27 +0000

    build based on 383f84a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastAlgebraicDataTypes","category":"page"},{"location":"#FastAlgebraicDataTypes","page":"Home","title":"FastAlgebraicDataTypes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastAlgebraicDataTypes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastAlgebraicDataTypes]","category":"page"}]
+}
